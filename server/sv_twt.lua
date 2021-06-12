059272e06ba8db3cd57b5036651865f30a94193e
SayNo <65409299+SayNo-Release@users.noreply.github.com>
2021-06-12 15:04:14 +0200

    Add files via upload

---
+++ server/sv_twt.lua
@@ -0,0 +1,38 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterCommand('twt', function(source, args, rawCommand)
+    local src = source
+	local msg = rawCommand:sub(5)
+	local args = msg
+    if player ~= false then
+        local name = getIdentity(source)
+	    fal = name.firstname .. " " .. name.lastname
+        local xPlayers	= ESX.GetPlayers()
+        for i=1, #xPlayers, 1 do
+            local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+			TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Twitter', ''..fal..'', ''..msg..'', 'CHAR_STRETCH', 0)
+        end
+    end
+end, false)
+
+function getIdentity(source)
+	local identifier = GetPlayerIdentifiers(source)[1]
+	local result = MySQL.Sync.fetchAll("SELECT * FROM users WHERE identifier = @identifier", {['@identifier'] = identifier})
+	if result[1] ~= nil then
+		local identity = result[1]
+
+		return {
+			identifier = identity['identifier'],
+			firstname = identity['firstname'],
+			lastname = identity['lastname'],
+			dateofbirth = identity['dateofbirth'],
+			sex = identity['sex'],
+			height = identity['height']
+			
+		}
+	else
+		return nil
+	end
+end
